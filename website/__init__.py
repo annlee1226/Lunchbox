088c0c3c93c3ca03d33a79acc42c88a1321b720f
Anna <alee531@ucr.edu>
2024-08-17 18:59:22 -0700

    Files

---
+++ website/__init__.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+import os
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY']  =  os.getenv('SECRET_KEY', 'default_key_if_not_set')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .auth import auth
+    from .pages import pages
+
+    app.register_blueprint(pages, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+    from .data import User
+
+    
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created Database!')
+
+
---
+++ website/pages.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .data import User
+from . import db
+import json
+
+pages = Blueprint('pages', __name__)
+
+
+@pages.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
